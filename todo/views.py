e15aba711c86fd62725477826b52c87b1cba6391
Muxlisa <muxlisaa1302@gmail.com>
2024-11-28 06:48:02 +0500

    no comment

---
+++ todo/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import HttpResponse
+
+def task_list():
+    html_response="""
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+    
+    
+    """
